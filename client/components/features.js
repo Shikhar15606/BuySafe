04e00f79be48f4a4db6f0fe21e3c8839bd497fc8
Shikhar15606 <shikharshukla15606@gmail.com>
2022-05-09 15:33:04 +0530

    Justify Content

--- client/components/features.js
+++ client/components/features.js
@@ -49,11 +49,11 @@ const Features = () => {
                   <div className='absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white'>
                     <feature.icon className='h-6 w-6' aria-hidden='true' />
                   </div>
-                  <p className='ml-16 text-lg leading-6 font-medium text-gray-900'>
+                  <p className='ml-16 text-lg leading-6 font-medium text-gray-900 '>
                     {feature.name}
                   </p>
                 </dt>
-                <dd className='mt-2 ml-16 text-base text-gray-500'>
+                <dd className='mt-2 ml-16 text-base text-gray-500 text-justify'>
                   {feature.description}
                 </dd>
               </div>
